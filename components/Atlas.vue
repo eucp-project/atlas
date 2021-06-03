087c148b92dcc85ef144d186efcf901617f985cc
Peter Kalverla <peter.kalverla@gmx.com>
2021-06-03 17:42:43 +0200

    Test green button deployment

--- components/Atlas.vue
+++ components/Atlas.vue
@@ -25,7 +25,7 @@
       </div>
 
       <div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="test">
+        <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="test">
           Button
         </button>
       </div>
