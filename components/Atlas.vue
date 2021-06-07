2115d22ccc0211d0a0c513999b2a6b9605a3f566
Peter Kalverla <peter.kalverla@gmx.com>
2021-06-07 09:39:44 +0200

    First version of prototype layout

--- components/Atlas.vue
+++ components/Atlas.vue
@@ -1,16 +1,32 @@
 <template>
-  <div>
-    <div>
+  <div class="flex-col m-0 p-2 h-screen">
+    <div class="flex place-content-center m-4">
+      <img src="~/static/eucp_logo.png" alt="EUCP Logo">
+      <h1 class="text-2xl">
+        EUCP WP2 - Atlas of constrained climate projections</h2>
+      </h1>
+    </div>
+    <div class="flex place-content-center space-x-3">
       <Dropdown v-model="selectedVariable" :options="variables" />
       <Dropdown v-model="selectedSeason" :options="seasons" />
       <Dropdown v-model="selectedPercentile" :options="percentiles" />
       <Dropdown v-model="selectedDataset" :options="datasets" />
-      <div id="main-content">
-        <img
-          :src="require(`~/assets/sample_figures/eur_${selectedVariable}_41-60_${selectedSeason}_${selectedDataset}_${selectedPercentile}perc.png`)"
-          alt="Map of climate projections for Europe"
-        >
-      </div>
+      <Dropdown v-model="selectedMethod" :options="methods" />
+    </div>
+    <div class="flex place-content-center">
+      <img
+        class="h-96"
+        :src="require(`~/assets/sample_figures/eur_${selectedVariable}_41-60_${selectedSeason}_${selectedDataset}_${selectedPercentile}perc.png`)"
+        alt="Map of climate projections for Europe"
+      >
+    </div>
+    <div class="flex place-content-end space-x-3">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        More info
+      </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Download data
+      </button>
     </div>
   </div>
 </template>
@@ -23,6 +39,7 @@ export default {
       selectedPercentile: '10',
       selectedSeason: 'djf',
       selectedVariable: 'tas',
+      selectedMethod: 'climwip',
       datasets: {
         cmip6: 'CMIP6'
       },
@@ -40,6 +57,9 @@ export default {
       variables: {
         tas: 'Temperature',
         pr: 'Precipitation'
+      },
+      methods: {
+        climwip: 'ClimWIP'
       }
     }
   }
