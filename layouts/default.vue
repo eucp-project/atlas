9adb11c68e1fd1cdfdcf68124c8dfad702801fa4
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-07-30 16:42:01 +0200

    add first draft of layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="container mx-auto m-0 p-2 h-screen max-w-4xl self-center">
+    <div class="flex place-content-left m-4">
+      <NuxtLink :to="`/`">
+        <img src="~/static/eucp_logo.png" alt="EUCP Logo">
+      </NuxtLink>
+    </div>
     <Nuxt />
   </div>
 </template>
