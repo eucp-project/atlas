b0dafe7ddd8c81005a0352e8a1b03afd81880bf3
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-08-02 16:23:25 +0200

    fix indentation

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,14 @@
       <NuxtLink :to="`/`">
         <img src="~/static/eucp_logo.png" alt="EUCP Logo">
       </NuxtLink>
-        <div class="flex m-4 justify-around items-center">
-          <NuxtLink :to="`/`">
-            <p class="text-sm text-gray-400"> Home </p>
-          </NuxtLink>
-            <input class="w-32 h-8 focus:shadow-outline rounded text-sm px-2 pl-4" type="search" placeholder="Search...">
-        </div>
+      <div class="flex m-4 justify-around items-center">
+        <NuxtLink :to="`/`">
+          <p class="text-sm text-gray-400">
+            Home
+          </p>
+        </NuxtLink>
+          <input class="w-32 h-8 focus:shadow-outline rounded text-sm px-2 pl-4" type="search" placeholder="Search...">
+      </div>
     </div>
       <Nuxt />
   </div>
