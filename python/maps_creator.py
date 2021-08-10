438eefb4915e985290f540f35b003a4ed161ad1d
Yang <y.liu@esciencecenter.nl>
2021-08-10 23:50:18 +0200

    check map type in the script

--- python/maps_creator.py
+++ python/maps_creator.py
@@ -3,7 +3,7 @@ Update content of the Atlas - generate maps based on the netcdf data
 Function        : Plot maps in a uniform way based on the netcdf files
 Author          : Team BETA
 First Built     : 2021.08.09
-Last Update     : 2021.08.09
+Last Update     : 2021.08.10
 Library         : os, glob, netcdf4, matplotlib, cartopy, argparse
 Description     : In this notebook serves to extract netcdf data and generate maps for Atlas page.
 Return Values   : png files
@@ -22,14 +22,24 @@ def maps(datapath, output_path, map_type):
     Generate maps in a uniform way based on the netcdf files.
     """
     # get the list of .nc files in the target directory
-    nc_files = glob.glob(os.path.join(datapath,"*.nc"))
-    print(nc_files)
-    print(output_path)
-    print(map_type)
+    #nc_files = glob.glob(os.path.join(datapath,"*.nc"))
+    all_files = os.listdir(datapath)
+    nc_files = [x for x in all_files if ".nc" in x]
+    # check if the map type is correct or not
+    if map_type == "prec":
+        print("Plot precipitation map.")
+    elif map_type == "tas":
+        print("Plot temperature map.")
+    else:
+        raise IOError("The chosen map type is not supported for plotting!")
+    # plot maps
+    for i in nc_files:
+        print("Processing file: {}".format(i))
+        plot(i, datapath, output_path, map_type)
 
-def plot():
+def plot(nc_file, ):
     """
-    Create plots
+    Plot precipitation / temperature
     """
 
 if __name__ == "__main__":
