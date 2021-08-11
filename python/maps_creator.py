fbfb74a5f7bde34bf9573056652ab621eaa27442
Yang <y.liu@esciencecenter.nl>
2021-08-11 09:44:12 +0200

    update script for batch processing of plotting

--- python/maps_creator.py
+++ python/maps_creator.py
@@ -24,7 +24,7 @@ def maps(datapath, output_path, map_type):
     # get the list of .nc files in the target directory
     #nc_files = glob.glob(os.path.join(datapath,"*.nc"))
     all_files = os.listdir(datapath)
-    nc_files = [x for x in all_files if ".nc" in x]
+    nc_files = [x for x in all_files if ".nc" in x] # get files with .nc
     # check if the map type is correct or not
     if map_type == "prec":
         print("Plot precipitation map.")
@@ -37,10 +37,71 @@ def maps(datapath, output_path, map_type):
         print("Processing file: {}".format(i))
         plot(i, datapath, output_path, map_type)
 
-def plot(nc_file, ):
+def plot(nc_file, datapath, output_path, map_type):
     """
     Plot precipitation / temperature
     """
+    # extract data - weighted (constrained) and unweighted (unconstrained)
+    dataset = Dataset(os.path.join(datapath, nc_file))
+    if map_type == "prec":
+        data_weight = dataset['pr_weighted'][:]
+        data_unweight = dataset['pr_unweighted'][:]
+        cmap = "BrBG"
+    elif map_type == "tas":
+        data_weight = dataset['tas_weighted'][:]
+        data_unweight = dataset['tas_unweighted'][:]
+        cmap = "coolwarm"
+    # latitudes and longitudes
+    lat = dataset['lat'][:]
+    lon = dataset['lon'][:]
+    # plot map - constrained
+    fig1 = plt.figure(figsize=(12.8,9.6))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([-10,40,30,75],ccrs.PlateCarree()) # East, West, South, Nouth
+    ax.coastlines(resolution='110m', color='black', linewidth=2)
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl.xlabels_top = False
+    gl.xlabels_bottom = True
+    gl.ylabels_left = True
+    gl.ylabels_right = False
+    gl.xlines = False
+    gl.ylines = False
+    gl.xlabel_style = {'size': 20, 'color': 'black'}
+    gl.ylabel_style = {'size': 20, 'color': 'black'}
+    cs = plt.pcolormesh(lon, lat, data_weight, cmap=cmap)
+                        #vmin=-100, vmax=50)
+    cbar = fig1.colorbar(cs, extend='both', orientation='horizontal',
+                        shrink =0.8, pad=0.08, spacing="uniform")
+    cbar.ax.tick_params(labelsize = 20)
+    ax.set_title('EUR relative precipitation difference (%)', fontsize=20)
+    plt.show()
+    fig1.savefig(os.path.join(output_path,"{}_constrained.png".format(nc_file[:-3])),
+                 dpi=150)
+    fig1.cloes()
+    # plot map - unconstrained
+    fig2 = plt.figure(figsize=(12.8,9.6))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([-10,40,30,75],ccrs.PlateCarree()) # East, West, South, Nouth
+    ax.coastlines(resolution='110m', color='black', linewidth=2)
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl.xlabels_top = False
+    gl.xlabels_bottom = True
+    gl.ylabels_left = True
+    gl.ylabels_right = False
+    gl.xlines = False
+    gl.ylines = False
+    gl.xlabel_style = {'size': 20, 'color': 'black'}
+    gl.ylabel_style = {'size': 20, 'color': 'black'}
+    cs = plt.pcolormesh(lon, lat, data_unweight, cmap=cmap)
+                        #vmin=-100, vmax=50)
+    cbar = fig2.colorbar(cs, extend='both', orientation='horizontal',
+                        shrink =0.8, pad=0.08, spacing="uniform")
+    cbar.ax.tick_params(labelsize = 20)
+    ax.set_title('EUR relative precipitation difference (%)', fontsize=20)
+    plt.show()
+    fig2.savefig(os.path.join(output_path,"{}_unconstrained.png".format(nc_file[:-3])),
+                 dpi=150)
+    fig2.cloes()
 
 if __name__ == "__main__":
     # define command line arguments
