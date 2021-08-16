a3ebf1912c9c3fa49f292c0806402866218a2a30
Peter Kalverla <peter.kalverla@gmx.com>
2021-08-16 14:55:22 +0200

    Found a working configuration but can probably still be cleaner

--- components/Atlas.vue
+++ components/Atlas.vue
@@ -1,28 +1,22 @@
 <template>
-  <div>
-    <div class="flex place-content-center m-4">
-      <h1 class="text-2xl align-middle">
-        EUCP WP2 - Atlas of constrained climate projections</h2>
-      </h1>
+  <div class="flex flex-col place-content-center items-center min-h-full h-full max-h-full border-4">
+    <h1 class="m-4 text-3xl">
+      EUCP WP2 - Atlas of constrained climate projections
+    </h1>
+    <div class="space-x-1">
+      <Dropdown v-model="selectedVariable" :options="variables" />
+      <Dropdown v-model="selectedSeason" :options="seasons" />
+      <Dropdown v-model="selectedPercentile" :options="percentiles" />
+      <Dropdown v-model="selectedDataset" :options="datasets" />
+      <Dropdown v-model="selectedMethod" :options="methods" />
+      <Dropdown v-model="selectedConstrained" :options="constrainedOptions" />
     </div>
-    <div class="flex flex-col place-content-center items-center">
-      <div class="space-x-1">
-        <Dropdown v-model="selectedVariable" :options="variables" />
-        <Dropdown v-model="selectedSeason" :options="seasons" />
-        <Dropdown v-model="selectedPercentile" :options="percentiles" />
-        <Dropdown v-model="selectedDataset" :options="datasets" />
-        <Dropdown v-model="selectedMethod" :options="methods" />
-        <Dropdown v-model="selectedConstrained" :options="constrainedOptions" />
-      </div>
-      <div class="overflow-auto">
-        <img
-          class="object-contain"
-          style="max-width:890px"
-          :src="require(`~/assets/sample_figures/eur_${selectedVariable}_41-60_${selectedSeason}_${selectedDataset}_${selectedPercentile}perc.png`)"
-          alt="Map of climate projections for Europe"
-        >
-      </div>
-    </div>
+    <div class="bg-center bg-no-repeat bg-contain flex-shrink flex-grow w-full border-4"  :style="{ backgroundImage: `url(${require('~/assets/sample_figures/eur_' + selectedVariable + '_41-60_' + selectedSeason + '_' + selectedDataset + '_' + selectedPercentile + 'perc.png')})` }" />
+    <!-- <img
+      class="object-contain h-full border-4"
+      :src="require(`~/assets/sample_figures/eur_${selectedVariable}_41-60_${selectedSeason}_${selectedDataset}_${selectedPercentile}perc.png`)"
+      alt="Map of climate projections for Europe"
+    > -->
     <div class="flex place-content-center space-x-3">
       <Button :text="`More info`" :target="`/about`" />
       <Button :text="`Download data`" />
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container mx-auto m-0 p-2 h-screen w-screen">
-    <div class="flex m-4 justify-around">
+  <div class="p-4 min-h-screen h-screen max-h-screen flex flex-col">
+    <div class="flex flex-row gap-4 justify-around">
       <NuxtLink :to="`/`">
         <img src="~/static/eucp_logo.png" alt="EUCP Logo">
       </NuxtLink>
-      <div class="flex m-4 justify-around items-center">
+      <div class="flex items-center">
         <NuxtLink :to="`/`">
           <p class="text-sm text-gray-400">
             Home
@@ -13,6 +13,6 @@
         <input class="w-32 h-8 focus:shadow-outline rounded text-sm px-2 pl-4" type="search" placeholder="Search...">
       </div>
     </div>
-    <Nuxt />
+    <Nuxt class="flex-grow flex-shrink border-4" />
   </div>
 </template>
