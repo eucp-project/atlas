d2b3744490e3c4a936ed1135a5eb96090308aa38
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-08-26 14:30:48 +0200

    edit text of popup box for each dropdown

--- components/Atlas.vue
+++ components/Atlas.vue
@@ -4,12 +4,12 @@
       EUCP WP2 - Atlas of constrained climate projections
     </h1>
     <div class="space-x-1">
-      <Dropdown v-model="selectedVariable" :options="variables" alttext="Select a variable" />
-      <Dropdown v-model="selectedSeason" :options="seasons" alttext="Select a season" />
-      <Dropdown v-model="selectedPercentile" :options="percentiles" alttext="Select a percentiles" />
-      <Dropdown v-model="selectedDataset" :options="datasets" alttext="Select a dataset" />
-      <Dropdown v-model="selectedMethod" :options="methods" alttext="Select a method" />
-      <Dropdown v-model="selectedConstrained" :options="constrainedOptions" alttext="Select a constrained" />
+      <Dropdown v-model="selectedVariable" :options="variables" alttext="Choose a variable." />
+      <Dropdown v-model="selectedSeason" :options="seasons" alttext="Select a season. Winter is DJF and summer is JJA." />
+      <Dropdown v-model="selectedPercentile" :options="percentiles" alttext="Percentiles indicate how likely these changes are." />
+      <Dropdown v-model="selectedDataset" :options="datasets" alttext="Select a dataset. Currently only CMIP6 is included." />
+      <Dropdown v-model="selectedMethod" :options="methods" alttext="Select a method. See More info for explanations of the methods." />
+      <Dropdown v-model="selectedConstrained" :options="constrainedOptions" alttext="Whether to display constrained or unconstrained projections." />
     </div>
     <div
       class="bg-center bg-no-repeat bg-contain flex-grow w-full"
