be7b75fc4e54102da0875ddb941776e51b0db15c
Yang <y.liu@esciencecenter.nl>
2021-10-01 17:14:49 +0200

    use correct climwip data for pre

--- python/maps_creator_atlas_data.py
+++ python/maps_creator_atlas_data.py
@@ -23,6 +23,7 @@ from textwrap import wrap
 # Generate images without having a window appear
 matplotlib.use('Agg')
 
+
 def files(datapath, output_path):
     """
     Generate maps in a uniform way based on the netcdf files.
@@ -30,11 +31,12 @@ def files(datapath, output_path):
     # get the list of .nc files in the target directory
     #nc_files = glob.glob(Path(datapath,"*.nc"))
     all_files = os.listdir(datapath)
-    nc_files = [x for x in all_files if ".nc" in x] # get files with .nc
+    nc_files = [x for x in all_files if ".nc" in x]  # get files with .nc
     # plot maps
     for i in nc_files:
         print("Processing file: {}".format(i))
         prepareData(i, datapath, output_path)
+
 
 def prepareData(nc_file, datapath, output_path):
     """
@@ -69,19 +71,21 @@ def prepareData(nc_file, datapath, output_path):
         for c in constrained:
             for p in percentile:
                 try:
-                    data_pr = dataset['pr'][key_s[s],key_c[c],key_p[p],:,:]
+                    data_pr = dataset['pr'][key_s[s], key_c[c], key_p[p], :, :]
                     # plot precipitation
                     plot(data_pr, lat, lon, "pr", project,
                          method, s, c, p, output_path)
                 except:
                     pass
                 try:
-                    data_tas = dataset['tas'][key_s[s],key_c[c],key_p[p],:,:]
+                    data_tas = dataset['tas'][key_s[s],
+                                              key_c[c], key_p[p], :, :]
                     # plot temperature
                     plot(data_tas, lat, lon, "tas", project,
                          method, s, c, p, output_path)
                 except:
                     pass
+
 
 def plot(data, lat, lon, variable, project, method,
          season, constrained, percentile, output_path):
@@ -89,13 +93,15 @@ def plot(data, lat, lon, variable, project, method,
     Plot relative precipitation and temperature using cartopy.
     """
     # name list
-    cons = ["uncons","cons"] # 0: unconstrained 1: constrained
+    cons = ["uncons", "cons"]  # 0: unconstrained 1: constrained
     # plot fig
-    fig = plt.figure(figsize=(12.8,9.6))
+    fig = plt.figure(figsize=(12.8, 9.6))
     ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.set_extent([-10,40,30,75],ccrs.PlateCarree()) # East, West, South, Nouth
+    # East, West, South, Nouth
+    ax.set_extent([-10, 40, 30, 75], ccrs.PlateCarree())
     ax.coastlines(resolution='110m', color='black', linewidth=2)
-    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=1, color='gray', alpha=0.5, linestyle='--')
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
+                      linewidth=1, color='gray', alpha=0.5, linestyle='--')
     gl.xlabels_top = False
     gl.xlabels_bottom = True
     gl.ylabels_left = True
@@ -107,19 +113,22 @@ def plot(data, lat, lon, variable, project, method,
     if variable == "pr":
         cs = plt.pcolormesh(lon, lat, data, cmap="BrBG", vmin=-50, vmax=50)
     elif variable == "tas":
-        cs = plt.pcolormesh(lon, lat, data, cmap="coolwarm", vmin=0, vmax=5)
+        cs = plt.pcolormesh(lon, lat, data, cmap="YlOrRd", vmin=0, vmax=5)
     cbar = fig.colorbar(cs, extend='both', orientation='vertical',
-                        shrink =0.8, pad=0.08, spacing="uniform")
-    cbar.ax.tick_params(labelsize = 20)
+                        shrink=0.8, pad=0.08, spacing="uniform")
+    cbar.ax.tick_params(labelsize=20)
     if variable == "pr":
-        ax.set_title("\n".join(wrap(f'{method} {cons[constrained]} {season.lower()} relative precipitation projections (%) - {percentile} percentile projected changes for 2050 with respect to present-day climate', 60)), fontsize=20)
+        ax.set_title("\n".join(wrap(
+            f'{method} {cons[constrained]} {season.lower()} relative precipitation projections (%) - {percentile} percentile projected changes for 2050 with respect to present-day climate', 60)), fontsize=20)
     elif variable == "tas":
-        ax.set_title("\n".join(wrap(f'{method} {cons[constrained]} {season.lower()} temperature projections (degC) - {percentile} percentile projected changes for 2050 with respect to present-day climate', 60)), fontsize=20)
-    #plt.show()
+        ax.set_title("\n".join(wrap(
+            f'{method} {cons[constrained]} {season.lower()} temperature projections (degC) - {percentile} percentile projected changes for 2050 with respect to present-day climate', 60)), fontsize=20)
+    # plt.show()
     fig.savefig(Path(output_path,
                 f"eur_{method}_{variable}_41-60_{season.lower()}_{project.lower()}_{percentile}perc_{cons[constrained]}.png"),
                 dpi=150)
     plt.close(fig)
+
 
 if __name__ == "__main__":
     # define command line arguments
