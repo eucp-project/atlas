4027c92ac1d200affabe115918bdb4512c00e687
geek-yang <geek-yang@users.noreply.github.com>
2021-10-19 07:46:49 +0000

    deploy: b504df364d1c25b6d26e901383c538312977d8a5

---
+++ _nuxt/ce46f60.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{490:function(t,n,e){"use strict";e.r(n);var l=e(40),component=Object(l.a)({},(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("Atlas")],1)}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Atlas:e(487).default})}}]);
