f6529d0536fbe87bbe1265ef9b549ce7500b3568
Peter Kalverla <peter.kalverla@gmx.com>
2021-10-29 12:12:57 +0200

    Don't link but clone

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div v-for="(map, i) in maps" :key="i">
+    <div v-for="(map, i) in maps" :key="i" class="m-1">
       <MapSelector v-model="map.path" />
       <button v-if="maps.length!=1" @click="maps.splice(i, 1)">Remove</button>
-      <button v-if="i===maps.length-1" @click="maps.push(map)">Add</button>
+      <button v-if="i===maps.length-1" @click="maps.push(clone(map))">Add</button>
     </div>
     <div class="flex">
       <img v-for="(map, id) in maps" :key="id" :src="map.path" :alt="id" class="h-96">
@@ -20,6 +20,11 @@ export default {
         { path: require('~/assets/processed_figures/eur_ASK_tas_41-60_djf_cmip6_50perc_cons.png') },
         { path: require('~/assets/processed_figures/eur_ASK_tas_41-60_djf_cmip6_90perc_cons.png') }
       ]
+    }
+  },
+  methods: {
+    clone (item) {
+      return JSON.parse(JSON.stringify(item))
     }
   }
 }
